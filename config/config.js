acc416bf5e43eb3aa17cb05d59ccb18049bc0124
steven hunt <st3v3n.hunt@gmail.com>
2018-04-09 15:51:28 +0100

    :notebook: Add description to each data source

--- config/config.js
+++ config/config.js
@@ -16,22 +16,27 @@ module.exports = {
     mergedDataFilename: getMergedDataFilename,
     sources: {
       csatNhs: {
+        description: 'Pharmacy Service (NHS) - Chlamydia screening and treatment',
         filename: 'csat-nhs-data.json',
         url: 'https://primarycare.blob.core.windows.net/etl-output/csat-nhs-data.json',
       },
       csatNonNhs: {
+        description: 'Pharmacy Service (Non-NHS) - Chlamydia screening and treatment',
         filename: 'csat-non-nhs-data.json',
         url: 'https://primarycare.blob.core.windows.net/etl-output/csat-non-nhs-data.json',
       },
       csu25: {
+        description: 'GSD Service - Chlamydia screening under 25s',
         filename: 'csu25-data.json',
         url: 'https://primarycare.blob.core.windows.net/etl-output/csu25-data.json',
       },
       pharmacy: {
+        description: 'Organisation - Pharmacy',
         filename: 'pharmacy-data.json',
         url: 'https://nhsukpharmacydataetl.blob.core.windows.net/etl-output/pharmacy-data.json',
       },
       shis: {
+        description: 'GSD Service - Sexual health information and support',
         filename: 'shis-data.json',
         url: 'https://primarycare.blob.core.windows.net/etl-output/shis-data.json',
       },
